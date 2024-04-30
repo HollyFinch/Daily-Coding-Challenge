de623ed0911e7a5eef33b7d840b135e71cf9ab33
Holly Finch <49234526+HollyFinch@users.noreply.github.com>
2024-04-30 17:16:22 +0100

    Add files via upload
    My quick answer to today's Daily Coding Problem challenge.... oops I might have "cheated" and pre-sorted the array 😉

---
+++ 30April.py
@@ -0,0 +1,34 @@
+# QUESTION 
+#published by Daily Coding Problem #1706 
+
+# Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+#For example, given [100, 4, 200, 1, 3, 2], the longest consecutive element sequence is [1, 2, 3, 4]. Return its length: 4.
+
+def findLongestStreak(arr):
+    
+    #I have "cheated" and pre-sorted the array
+    arr.sort()
+    length = len(arr)
+    streakCounter = 1
+    longestStreak = 1
+
+    for i in range(length - 1):
+        
+        if arr[i] == arr[i+1]:
+            continue
+        if arr[i+1] == arr[i] + 1:  # Check if the next element is one more than the current element
+            streakCounter += 1  # Increment streak counter
+        else:
+            streakCounter = 1  # Reset streak counter if not part of the streak
+        
+        if streakCounter > longestStreak:
+            longestStreak = streakCounter
+
+    return longestStreak
+
+# Example usage:
+arr = [100, 4, 200, 1, 3, 2]
+longest_streak = findLongestStreak(arr)
+print("Longest streak length:", longest_streak)
+
+
