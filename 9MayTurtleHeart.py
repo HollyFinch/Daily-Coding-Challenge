720cf8c4abb1da54b0dc3b228b83f2dd2c7b8602
Holly Finch <49234526+HollyFinch@users.noreply.github.com>
2024-05-09 18:23:46 +0100

    Add files via upload
    draw half of heart

--- 9MayTurtleHeart.py
+++ 9MayTurtleHeart.py
@@ -5,18 +5,20 @@ import turtle
 pencil = turtle.Turtle()
 pencil.shape("turtle")
 
-pencil.color("red")
-pencil.setheading(90)
+def draw_half_of_heart():
+    pencil.color("red")
+    pencil.setheading(90)
 
-for i in range(20):
-    pencil.forward(1)
-    pencil.right(3)
+    for i in range(20):
+        pencil.forward(1)
+        pencil.right(3)
 
-pencil.circle(50, 90)
-pencil.circle(10, 90)
-pencil.setheading(-120)
-pencil.circle(50, 90)
-pencil.circle(10, 90)
+    pencil.circle(50, 90)
+    pencil.circle(20, 100)
+    pencil.setheading(-120)
+    pencil.circle(100, 10)
+    pencil.right(160)
+
 
 #to be continued. This is a loop, not a heart
 
