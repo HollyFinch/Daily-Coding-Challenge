fdf3797e8ee823f2354ab8238504a35d4bd2d0dc
Holly Finch <49234526+HollyFinch@users.noreply.github.com>
2024-05-28 12:36:12 +0100

    Add files via upload
    So neat, unlike "traditional" array and matrix manipulation

--- 25May.py
+++ 25May.py
@@ -5,6 +5,8 @@ import numpy as np
 dictionary = {'no':'no', 'yes':'yes'}
 
 crosswordMx = np.array([['n','p'], ['o', 'k']])
+#shape returns size of the matrix, 2 by 2 in this case, so use either [0] or [1]
+Mx_dimension = crosswordMx.shape[0]
 
 for key, value in dictionary.items():
     print(f"Key: {key}")
@@ -17,6 +19,12 @@ for key, value in dictionary.items():
             for position in letter_position:
                 row,col = position
                 print(f"Element '{check_element}' found at position: ({row}, {col})")
+                
+                if (row,col) == (0,0):
+                    print(f"the letter '{check_element}' is in top left corner")
+                
+                if (row,col) == (Mx_dimension -1, Mx_dimension -1):
+                    print(f"the letter '{check_element}' is in bottom right corner")
         
         else: print("not in my matrix")
         
