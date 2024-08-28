201ede3c78965503c05ea8d2fe15d04eebc05add
Holly Finch <49234526+HollyFinch@users.noreply.github.com>
2024-08-28 18:52:44 +0100

    Update hello.php
    added more operators

--- hello.php
+++ hello.php
@@ -7,14 +7,22 @@ function calc($input1, $input2, $operator)
             return $input1 + $input2;
         case "-":
             return $input1 - $input2;
+        case "*":
+            return $input1 * $input2;
+        case "/":
+            return $input2 != 0 ? $input1 / $input2 : "BEEEEEEP!!!! DIVISION BY ZERO";
+        case "%":
+            return $input1 % $input2;
+        case "√":
+            return sqrt($input1);
         default:
             return "BEEEEEEP!!!! INVALID OPERATOR";
     }
 }
 
-$operator = readline("Please type + or - : ");
+$operator = readline("Please type +, -, *, /, %, or √ : ");
 $input1 = readline("Please enter the first value: ");
-$input2 = readline("Please enter the second value: ");
+$input2 = ($operator !== "√") ? readline("Please enter the second value: ") : null;
 
 $result = calc($input1, $input2, $operator);
 
